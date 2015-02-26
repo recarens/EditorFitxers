c4fbf180c15e06e3cc276f87ab57c8216e574d9e
recarens <eric.recarens@gmail.com>
2015-02-26 16:24:10 +0100

    Retocs i editan classe Main

--- app/src/main/java/cat/montilivi/editorfitxers/MainActivity.java
+++ app/src/main/java/cat/montilivi/editorfitxers/MainActivity.java
@@ -4,9 +4,20 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements View.OnClickListener{
+
+    final String NOM_FITXER = "productes.txt";
+    Button btnRead;
+    Button btnOpen;
+    Button btnSave;
+    EditText etNom;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,26 +25,8 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
     }
 
-
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    public void onClick(View v) {
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
